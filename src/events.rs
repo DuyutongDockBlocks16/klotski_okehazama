55fc6e4c751905da3d4aa22220230fb2003c0669
YutongDu <yutong.du@aalto.fi>
2025-01-18 16:05:33 +0200

    add event

---
+++ src/events.rs
@@ -0,0 +1,18 @@
+use hecs::Entity;
+
+#[derive(Debug)]
+pub struct BlockMoved {
+    pub entities: Vec<Entity>,
+}
+
+#[derive(Debug)]
+pub enum Event {
+    // Fired when the player hits an obstacle like a wall
+    BlockHitObstacle,
+
+    // Fired when an entity is moved
+    BlockMoved(EntityMoved),
+
+    // Fired when the box is placed on a spot
+    TargetBlockReachExit,
+}
